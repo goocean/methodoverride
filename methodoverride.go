bc74c94a2cc905c5382901c9cbbd19f1af7e46f7
fundon <cfddream@gmail.com>
2014-12-18 17:17:45 +0800

    update

--- methodoverride.go
+++ methodoverride.go
@@ -9,10 +9,11 @@ const (
 
 var Methods = []string{"PUT", "PATCH", "DELETE"}
 
-func New() {
+func New() func(http.Handler) http.Handler {
 	return MethodOverride
 }
 
+// Check methods
 func supports(m string) bool {
 	for _, v := range Methods {
 		if v == m {
@@ -22,7 +23,8 @@ func supports(m string) bool {
 	return false
 }
 
-func getMethod() (m string) {
+// Get `_method` From Head Or Form
+func getMethod(r *http.Request) (m string) {
 	m = r.Header.Get(HEAD_X_HTTP_METHOD_OVERRIDE)
 	if m == "" {
 		m = r.FormValue(PARAM_HTTP_METHOD_OVERRIDE)
@@ -30,10 +32,11 @@ func getMethod() (m string) {
 	return
 }
 
-func MethodOverride(h Handler) http.Handler {
+// Method Override Handler
+func MethodOverride(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
-			m := getMethod()
+			m := getMethod(r)
 			if m != "" {
 				if supports(m) {
 					r.Method = m
